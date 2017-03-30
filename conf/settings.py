b0e82bc926efde13be02294a9523e59f4fbb104d
wangyufu <mrwangyufu@163.com>
2017-03-30 17:58:06 +0800

    update

---
+++ conf/settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+DATABASE_TYPE = 'mysql'
+
+DATABASE_CONN = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'passwd': '123456',
+    'db': 'host_manage'
+}
